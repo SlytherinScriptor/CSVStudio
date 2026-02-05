84d42acff23e33952fe5212107af96eb2b7c9049
SlytherinScriptor <159412730+SlytherinScriptor@users.noreply.github.com>
2026-02-05 17:28:20 +0530

    Align sidebar and welcome styling across themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
 import { UpsertPanel } from './components/UpsertPanel';
 import { DeletePanel } from './components/DeletePanel';
 import { ComparePanel } from './components/ComparePanel';
--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './Welcome.css';
+import '../styles/Welcome.css';
 import { Footer } from './ui/Footer';
 
 interface WelcomeScreenProps {
